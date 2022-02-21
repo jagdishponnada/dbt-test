d8be88d4197dbb9418ce3f58101a0a5a6fdd2a98
jagdishponnada <subsofjag@gmail.com>
2022-02-21 20:34:45 +0100

    Build our first project

---
+++ models/stg_customers.sql
@@ -0,0 +1,6 @@
+select
+       customer_id,
+        first_name,
+        last_name
+
+    from `dbt-test-341917`.jaffle_shop.customer
---
+++ models/stg_orders.sql
@@ -0,0 +1,7 @@
+    select
+        order_id,
+        customer_id,
+        order_date,
+        status
+
+    from `dbt-test-341917`.jaffle_shop.orders
